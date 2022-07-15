834153b9c806ab3d146e892078ace085ec0cd149
vdenef <vdenef@users.noreply.github.com>
2022-07-15 11:40:46 -0400

    Update miseqR.R

--- R/miseqR.R
+++ R/miseqR.R
@@ -8,12 +8,31 @@
 # 1) taking proportions,
 # 2) multiplying by a given library size of n
 # 3) rounding down
-scale_reads <- function(physeq, n) {
-  physeq.scale <-
-    transform_sample_counts(physeq, function(x) {
-      (n * x/sum(x))
-    })
-  otu_table(physeq.scale) <- floor(otu_table(physeq.scale))
+##scale_reads <- function(physeq, n) {
+##  physeq.scale <-
+##    transform_sample_counts(physeq, function(x) {
+##      (n * x/sum(x))
+##    })
+##  otu_table(physeq.scale) <- floor(otu_table(physeq.scale))
+##  physeq.scale <- prune_taxa(taxa_sums(physeq.scale) > 0, physeq.scale)
+##  return(physeq.scale)
+scale_reads <- function(physeq, n = min(sample_sums(physeq)), round = "round") {
+  
+  # transform counts to n
+  physeq.scale <- transform_sample_counts(physeq, 
+                                          function(x) {(n * x/sum(x))}
+  )
+  
+  # Pick the rounding functions
+  if (round == "floor"){
+    otu_table(physeq.scale) <- floor(otu_table(physeq.scale))
+  } else if (round == "round"){
+    otu_table(physeq.scale) <- round(otu_table(physeq.scale))
+  } else if (round == "matround"){
+    otu_table(physeq.scale) <- matround(otu_table(physeq.scale))
+  }
+  
+  # Prune taxa and return new phyloseq object
   physeq.scale <- prune_taxa(taxa_sums(physeq.scale) > 0, physeq.scale)
   return(physeq.scale)
 }
